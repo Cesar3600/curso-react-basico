6c7bb77006652266782c7a773cac414446f70639
cesar <cesar.contreras.medina.3600@gmail.com>
2020-02-22 15:39:10 -0500

    eliminado archivos de badges, editado App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
+
 import React from 'react'
-import BadgesNew from '../pages/BadgesNew'
-import Badge from '../pages/Badge'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Layout from './layout'
-import notFound from './notfound'
 
 
 const App = () => {
   return(
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path='/badges' component={Badge}/>
-          <Route exact path='/badges/new' component={BadgesNew}/>
-          <Route component={notFound}/>
-        </Switch>  
-      </Layout>
-    </Router>
+    <h3>Soy App</h3>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
