545f785ade11e00770b489ac14f837d223ad02ab
cesar <cesar.contreras.medina.3600@gmail.com>
2020-02-24 02:28:38 -0500

    agregado carga de data por API, loader, logica para cargar mas cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,39 @@
 
-import React from 'react'
+import React,{Component} from 'react'
 import Card from './card'
-const url = 'https://rickandmortyapi.com/api/character/'
+import Loader from 'react-loader-spinner'
 
 
-class App extends React.Component {
-
-  constructor(props){
-    super(props)
+class App extends Component{
+  constructor(){
+    super()
     this.state = {
-      data:[]
+      data: [],
+      error: null,
+      loading: true,
+      nropage: 1
     }
   }
 
-  asyncData = async() => {
-    await fetch(url)
+  asyncData = async () => {
+    this.setState({
+      loading: true,
+      error: null
+    })
+    try{
+      await fetch(`https://rickandmortyapi.com/api/character/?page=${this.state.nropage}`)
       .then(response => response.json())
-      .then(data => this.setState({data:data.results}))
+      .then(data => this.setState({
+        data:this.state.data.concat(data.results),
+        nropage: this.state.nropage + 1,
+        loading: !this.state.loading
+      }))
+    }
+    catch(err){
+      this.setState({
+        error:err
+      })
+    }
   }
 
   componentDidMount(){
@@ -30,9 +47,23 @@ class App extends React.Component {
           <div className="col-md-12">
             <div className="list-characters">
               {
-                this.state.data.map( info => <Card data={info}/>)
+                this.state.data.map(alias => {
+                  return <Card data={alias} id={alias.id}/>
+                })
               }
             </div>
+            {
+              this.state.loading && <Loader 
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                timeout={3000}/>
+            }
+            {
+              !this.state.loading && <button className="btn btn-info" onClick={() => this.asyncData()}>Add Characters</button>
+            }
+
           </div>
         </div>
   
@@ -42,6 +73,99 @@ class App extends React.Component {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import Card from './card'
+// import Loader from 'react-loader-spinner'
+
+
+// class App extends React.Component {
+
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       loading:true,
+//       error:null,
+//       data:[],
+//       nextPage:1
+//     }
+//   }
+
+//   asyncData = async() => {
+//     this.setState({loadind: true, error:null})
+//     const {nextPage,data} = this.state
+//     try{
+//       await fetch(`https://rickandmortyapi.com/api/character//?page=${nextPage}`)
+//         .then(response => response.json())
+//         .then(data => this.setState({
+//           data:this.state.data.concat(data.results),
+//           loading:false,
+//           nextPage: nextPage + 1
+//         }))
+//     } catch (err){
+//         this.setState({
+//           error:err
+//         })
+//     }
+//   }
+
+//   componentDidMount(){
+//     this.asyncData()
+//   }
+
+//   render(){
+//     return(
+//       <div className="container-fluid">
+//         <div className="row">
+//           <div className="col-md-12">
+//             <div className="list-characters">
+//               {
+//                 this.state.data.map( info => <Card data={info}/>)
+//               }
+//               {
+//                 this.state.loading && <Loader
+//                 type="Puff"
+//                 color="#00BFFF"
+//                 height={100}
+//                 width={100}
+//                 timeout={3000}
+//              />
+//               }
+//             </div>
+//             <button onClick={()=>this.asyncData()}className="btn btn-primary">Add more characters!</button>
+//           </div>
+//         </div>
+  
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
 
 
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,76 @@
+
 import React from 'react'
+
 const Card = props => {
-  const {name,image,gender,species,status} = props.data
+  const {name,image,gender,species} = props.data
   return(
-    <div className="card character mb-5 p-3">
+    <div className="card character p-3 mb-1">
       <img src={image} className="card-img-top" alt={name}/>
       <div className="card-body">
-        <h5 className="card-title text-center">{name}</h5>
+        <h5 className="card-title">{name}</h5>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item"><b>Genero: </b>{gender}</li>        
-        <li className="list-group-item"><b>Specie: </b>{species}</li>
-        <li className="list-group-item"><b>Status: </b>{status}</li>
+        <li className="list-group-item">{gender}</li>
+        <li className="list-group-item">{species}</li>
       </ul>
     </div>
   )
 }
 
 export default Card
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// const Card = props => {
+//   const {name,image,gender,species,status} = props.data
+//   return(
+//     <div className="card character mb-5 p-3">
+//       <img src={image} className="card-img-top" alt={name}/>
+//       <div className="card-body">
+//         <h5 className="card-title text-center">{name}</h5>
+//       </div>
+//       <ul className="list-group list-group-flush">
+//         <li className="list-group-item"><b>Genero: </b>{gender}</li>        
+//         <li className="list-group-item"><b>Specie: </b>{species}</li>
+//         <li className="list-group-item"><b>Status: </b>{status}</li>
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default Card
 
 
